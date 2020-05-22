a56d35bc0c0fc569a835b894b1f163c9694f6abe
David Paquette <dave@davepaquette.com>
2020-05-22 16:01:25 -0600

    End state

--- Startup.cs
+++ Startup.cs
@@ -30,9 +30,12 @@ namespace FeatureManagementTargetingSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSingleton<ITargetingContextAccessor, TargetingContextAccesor>();
             services.AddFeatureManagement()
                 .AddFeatureFilter<TimeWindowFilter>()
-                .AddFeatureFilter<PercentageFilter>();
+                .AddFeatureFilter<PercentageFilter>()
+                .AddFeatureFilter<TargetingFilter>();
             services.AddScoped<BlogService>();
 
             services.AddDbContext<ApplicationDbContext>(options =>
---
+++ TargetingContextAccesor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.FeatureManagement.FeatureFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeatureManagementTargetingSample
+{
+    public class TargetingContextAccesor : ITargetingContextAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TargetingContextAccesor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public ValueTask<TargetingContext> GetContextAsync()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            var result = new TargetingContext();
+            result.UserId = httpContext.User.Identity.Name;
+
+            if (httpContext.User.HasClaim("Insider", "true"))
+            {
+                result.Groups = new string[] { "Insiders" };
+            }
+            return new ValueTask<TargetingContext>(result);
+        }
+
+    }
+}
